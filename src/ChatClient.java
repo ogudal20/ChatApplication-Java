743b74532ca9c5e9997583fb90d12bb48209360f
ogudal20 <omar_gudal@hotmail.co.uk>
2018-11-30 18:02:17 +0000

    Update ChatClient.java

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -9,51 +9,35 @@ import java.net.*;
 
 public class ChatClient {
 
-	//First thing we will need a JFrame container to hold
-	//the all the other containers.
-	//All the swing components will left as static as all the instances will be left
-	//constant.
+
 	static JFrame chatWindow = new JFrame("Chat Application");
-	//Now we add text area for the chat messages to be displayed on.
-	//JtextArea contains two parameters one for the row and the other columns
 	static JTextArea chatArea = new JTextArea(22, 40);
-	//We will need a textfield for the user to enter in the messages.
-	//One parameter for columns.
 	static JTextField textField = new JTextField(40);
-	//Next will have a line to separate the chat area and  textfield.
 	static JLabel blankLabel = new JLabel("		");
-	//Now we need a send button. To send out message.
 	static JButton sendButton = new JButton("Send");
-	static BufferedReader in;	//Take input from socket stream.
-	static PrintWriter out;	//display output to socket stream.
+	static BufferedReader in;	
+	static PrintWriter out;	
 	static JLabel nameLabel = new JLabel("		");
 	
 	//Adding components to chat window.
 	ChatClient(){
 		
-		//How are components we be arranged on the JFrame.
+		
 		chatWindow.add(nameLabel);
 		chatWindow.setLayout(new FlowLayout());
-		//Add the scroll bar to the chat area.
 		chatWindow.add(new JScrollPane(chatArea));
 		chatWindow.add(blankLabel);
 		chatWindow.add(textField);
 		chatWindow.add(sendButton);
 		
-		//Close button on GUI.Stops the application
 		chatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		chatWindow.setSize(475, 500);	//Width and height
-		chatWindow.setVisible(true); //Display visible to true.
+		chatWindow.setSize(475, 500);	
+		chatWindow.setVisible(true); 
 		
-		//We want only the user to enter text if they have established a connection with the server.
+		
 		textField.setEditable(false);
-		//the chat area only is there to display messages and not for entering messages.
 		chatArea.setEditable(false);
-		//Now we bind the send button to the listener.
-		//Now when the user clicks send the message will be sent
-		//to the server.
 		sendButton.addActionListener(new Listener());
-		//Now when the user clicks enter the user should still be able to send messages to the server.
 		textField.addActionListener(new Listener());
 		
 		
@@ -76,8 +60,7 @@ public class ChatClient {
 				"IP Address Required: ",
 				JOptionPane.PLAIN_MESSAGE);
 		
-		//Socket object with ipaddress above and port number.
-		//connecting to the server.
+		
 		Socket soc = new Socket(ipAddress, 6969);
 		in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 		out = new PrintWriter(soc.getOutputStream(), true);
@@ -86,11 +69,10 @@ public class ChatClient {
 		
 		while(true){
 			
-			//Get the name of the user.
-			//First we get capture message the server sends us.
+			
 			String str = in.readLine();
 			
-			//Now we create a dialog box for the user to enter a name.
+			
 			if(str.equals("NAMEREQUIRED")){
 				String name = JOptionPane.showInputDialog(
 						chatWindow,
@@ -98,11 +80,11 @@ public class ChatClient {
 						"Name Required!!",
 						JOptionPane.PLAIN_MESSAGE);
 				
-				//Get the name and send it to the server.
+				
 				out.println(name);
 				
 				
-				//Now we check when the name already exists.
+				
 			}else if(str.equals("NAMEALREADYEXISTS")){
 				String name = JOptionPane.showInputDialog(
 						chatWindow, 
@@ -110,14 +92,13 @@ public class ChatClient {
 						"Name Already Exists!!", 
 						JOptionPane.WARNING_MESSAGE);
 				
-				//Now we check for when the name is accepted.
+				
 				//startsWith. = This is because the server will send both the name and NAMEACCPEPTED
 			}else if(str.startsWith("NAMEACCEPTED")){
 				
 				//After the name is accepted the user can enter in text into the 
 				//message box.
 				textField.setEditable(true);
-				//Now we will have to separate the name and NAMEACCEPTED
 				nameLabel.setText("You are logged in as: " +str.substring(12));
 				
 			}else{
